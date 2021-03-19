33592abed49d1390bddea94f6a13a4392c000989
Elliott Vargo <vargoelliott@gmail.com>
2021-03-19 23:27:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,14 +7,9 @@ def initialize(word)
 @word = word
 end
 
+
 def match(some_array)
   @some_array = some_array
-end
-
-def compare
-end
-
-def find_anagrams
     @word.split("").collect do |letter_1| #split word into letters and operate on each letter
       result = letter_1.match {@some_array.each { |x| x.split("")}}
     end
